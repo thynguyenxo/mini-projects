c9970e3a54f1a51d99b17b70500c06cbc0a47392
Thy Nguyen <thynguyenxo@gmail.com>
2023-06-30 19:13:46 -0700

    fixed buttons bug

--- restaurant-page/src/about.js
+++ restaurant-page/src/about.js
@@ -9,7 +9,6 @@ function aboutUs() {
   const image = document.createElement('img');
   image.setAttribute('id', 'main-img');
   image.src = 'https://images.unsplash.com/photo-1629407119384-d42320c3e576?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80';
-  image.height = '600'; // TODO: remove this when styling
   about.appendChild(image);
 
   const para = document.createElement('p');
